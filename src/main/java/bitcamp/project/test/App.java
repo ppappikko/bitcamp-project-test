27af58f7886427e506556319a07670dcfea006fb
Heewookji <wlgmldnr@daum.net>
2019-04-17 16:00:49 +0900

    .

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -5,6 +5,7 @@ package bitcamp.project.test;
 
 import bitcamp.project.test.service.Heewook;
 import bitcamp.project.test.service.StudyMemberService;
+import bitcamp.project.test.service.sangmin;
 
 public class App {
   public String getGreeting() {
@@ -17,6 +18,12 @@ public class App {
     h.list();
     System.out.println("-------------------------------------------");
  
+    System.out.println("⎝༼ ◕-д-◕ ༽⎠⎝༼ ◕-д-◕ ༽⎠");
+    sangmin s = new sangmin();
+    s.list();
+    System.out.println("⎝༼ ◕-д-◕ ༽⎠⎝༼ ◕-д-◕ ༽⎠");
+    
+    
     System.out.println("***********************");
     StudyMemberService sms = new StudyMemberService();
     sms.list();
