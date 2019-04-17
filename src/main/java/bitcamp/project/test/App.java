963ce13a2dbb470d4b9702e02ef2d5ed4212cf00
hyenny <simssbook9@naver.com>
2019-04-17 15:55:54 +0900

    스터디종류 목록 추가

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -3,12 +3,17 @@
  */
 package bitcamp.project.test;
 
+import bitcamp.project.test.service.StudyService;
+
 public class App {
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+      System.out.println("--------------------------------------");
+      StudyService studentService = new StudyService();
+      studentService.list("스프링");
+      System.out.println("--------------------------------------");
     }
 }
