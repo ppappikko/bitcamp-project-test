010e19c5c3704b88a22098969c7631d5230b5e13
Sangmin Park <sangmin.p42@gmail.com>
2019-04-17 16:22:31 +0900

    연습

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -31,16 +31,9 @@ public class App {
     sms.list();
     System.out.println("***********************");
     /*---------------------------------------------------------*/
-<<<<<<< HEAD
-=======
-    
->>>>>>> branch 'master' of https://github.com/ppappikko/bitcamp-project-test.git
     System.out.println("1234");
-<<<<<<< HEAD
     
     System.out.println("병합 연습");
-=======
     System.out.println("뇌정지온다");
->>>>>>> branch 'master' of https://github.com/ppappikko/bitcamp-project-test.git
   }
 }
