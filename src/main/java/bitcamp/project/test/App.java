77abbcbe438460ad086169a0b4357c6125305cac
hyenny <simssbook9@naver.com>
2019-04-17 16:23:32 +0900

    ㅎ

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -3,14 +3,19 @@ package bitcamp.project.test;
 import bitcamp.project.test.service.Heewook;
 import bitcamp.project.test.service.Jihwan;
 import bitcamp.project.test.service.StudyMemberService;
-import bitcamp.project.test.service.sangmin;
 import bitcamp.project.test.service.StudyService;
+import bitcamp.project.test.service.sangmin;
 
 public class App {
   public String getGreeting() {
     return "Hello world.";
   }
   public static void main(String[] args) {
+    
+    StudyService studyService = new StudyService();
+    studyService.list("git");
+    System.out.println("지우지 마세요ㅠㅠ..");
+    
     System.out.println("졸립다..");
     Jihwan jihwan = new Jihwan();
     jihwan.list();
@@ -34,6 +39,4 @@ public class App {
     /*---------------------------------------------------------*/
   }
     
-    System.out.println("1234");
-  }
 }
