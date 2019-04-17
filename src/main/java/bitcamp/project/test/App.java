0d6a6cec4cf3acf4471168c7f1c2052f4f5f89ec
hyunwoo <hwim1202@gmail.com>
2019-04-17 17:16:08 +0900

    aa

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -1,7 +1,9 @@
 package bitcamp.project.test;
 
 import bitcamp.project.test.service.Heewook;
+import bitcamp.project.test.service.Jihwan;
 import bitcamp.project.test.service.StudyMemberService;
+import bitcamp.project.test.service.StudyService;
 import bitcamp.project.test.service.sangmin;
 
 public class App {
@@ -9,24 +11,38 @@ public class App {
     return "Hello world.";
   }
   public static void main(String[] args) {
-    System.out.println("------------------------------");
-    StudyMemberService s = new StudyMemberService();
-    s.list();
-    System.out.println("------------------------------");
+    
+    System.out.println("-------------------------------------------");
+    StudyService studyService = new StudyService();
+    studyService.list("git");
+    System.out.println("지우지 마세요ㅠㅠ..");
+    System.out.println("-------------------------------------------");
+    
+    System.out.println("졸립다..");
+    Jihwan jihwan = new Jihwan();
+    jihwan.list();
+    System.out.println("졸립다..");
+    /*---------------------------------------------------------*/
     System.out.println("-------------------------------------------");
     Heewook h = new Heewook();
     h.list();
     System.out.println("-------------------------------------------");
-
-    sangmin a = new sangmin();
-    a.list();
+    /*---------------------------------------------------------*/
     System.out.println("⎝༼ ◕-д-◕ ༽⎠⎝༼ ◕-д-◕ ༽⎠");
+    sangmin s = new sangmin();
+    s.list();
+    System.out.println("⎝༼ ◕-д-◕ ༽⎠⎝༼ ◕-д-◕ ༽⎠");
+    /*---------------------------------------------------------*/
 
     System.out.println("***********************");
     StudyMemberService sms = new StudyMemberService();
     sms.list();
     System.out.println("***********************");
-    
+    /*---------------------------------------------------------*/
     System.out.println("1234");
+    
+    System.out.println("병합 연습");
+    System.out.println("뇌정지온다");
   }
+
 }
